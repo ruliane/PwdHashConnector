b0a618a018a797c6b0bc2d5948a832c067727dac
ruliane <ruliane+github@ruliane.net>
2022-11-23 16:08:51 +0100

    Tag samAccountName and objectSid as ImportuOnly

--- pwdhash-schema.ps1
+++ pwdhash-schema.ps1
@@ -3,8 +3,8 @@ $obj = New-Object -Type PSCustomObject
 @(
 	@{ Name="objectClass"; 			Type="String"; Value="person" }
     @{ Name="Anchor-objectGUID"; 	Type="Binary"; Value="" }
-	@{ Name="samAccountName"; 		Type="String"; Value="" }
-	@{ Name="objectSid"; 			Type="Binary"; Value="" }
+	@{ Name="ImportOnly-samAccountName"; 		Type="String"; Value="" }
+	@{ Name="ImportOnly-objectSid"; 			Type="Binary"; Value="" }
 	@{ Name="nTHash"; 				Type="Binary"; Value="" }
 ) | foreach { `
 	$obj | Add-Member -Type NoteProperty -Name "$($_.Name)|$($_.Type)" -Value $_.Value
